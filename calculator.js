97e4a3f8e14673a62fcedbe42550a6f50ff8eae1
Alberto de Olivira <betodeoliveira@outlook.com>
2024-10-03 11:03:32 -0300

    update

--- calculator.js
+++ calculator.js
@@ -36,7 +36,9 @@ $("input").on("keydown", function (event) {
         return;
     }
     switch ($(this).attr("display-type")) {
-        case "percentage" || "currency":
+        case "percentage":
+        case "currency":
+        case "number":
             /* Allow: backspace, delete, tab, escape, and enter */
             if ($.inArray(event.keyCode, [46, 8, 9, 27, 13]) !== -1 ||
                 /* Allow: Ctrl+A */
@@ -62,7 +64,9 @@ $("input").on("keydown", function (event) {
 /* Check if the content that is being pasted is supported by the input */
 $("input").on("paste", function (event) {
     switch ($(this).attr("display-type")) {
-        case "percentage" || "currency":
+        case "percentage":
+        case "currency":
+        case "number":
             /* Get the pasted text */
             let _pastedText = event.originalEvent.clipboardData.getData('text');
             /* Remove non-numeric characters */
@@ -75,10 +79,26 @@ $("input").on("paste", function (event) {
     }
 });
 
+function checkInputAttributes(input) {
+
+    if (isNaN(parseFloat(input.val()))) {
+        input.val(0);
+    }
+
+    if (input.attr("number-max")) {
+        if (parseFloat(input.val()) > parseFloat(input.attr("number-max"))) {
+            console.log('max');
+            input.val(input.attr("number-max"));
+        }
+    }
+}
+
 /* On blur gets the internal value and format it to display as the input value */
 $("input").on("blur", function () {
     /* First check if the input has a value val and if not reset to default */
     if ($(this).val() == "") { $(this).val($(this).attr("default-value")); }
+    // Check for custom attributes
+    checkInputAttributes($(this));
     /* Save the val */
     $(this).attr('int-value', $(this).val());
     /* Format and update the input val */
@@ -88,6 +108,9 @@ $("input").on("blur", function () {
             break;
         case "currency":
             $(this).val(formatCurrency($(this).val()));
+            break;
+        case "number":
+            $(this).val(formatNumber($(this).val()));
             break;
     }
     if (isInitialized) {
